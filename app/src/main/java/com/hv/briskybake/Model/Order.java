d7bba4fe232635c4be9138ec84ea8a92e44c3730
VINEETA <vineetasharma882@gmail.com>
2021-04-20 15:14:43 +0530

    commit no1

--- app/src/main/java/com/hv/briskybake/Model/Order.java
+++ app/src/main/java/com/hv/briskybake/Model/Order.java
@@ -1,4 +1,60 @@
 package com.hv.briskybake.Model;
 
 public class Order {
+    private String ProductId;
+    private String ProductNmae;
+    private String Quality;
+    private String Price;
+    private String Discount;
+
+    public Order() {
+    }
+
+    public Order(String productId, String productNmae, String quality, String price, String discount) {
+        ProductId = productId;
+        ProductNmae = productNmae;
+        Quality = quality;
+        Price = price;
+        Discount = discount;
+    }
+
+    public String getProductId() {
+        return ProductId;
+    }
+
+    public void setProductId(String productId) {
+        ProductId = productId;
+    }
+
+    public String getProductNmae() {
+        return ProductNmae;
+    }
+
+    public void setProductNmae(String productNmae) {
+        ProductNmae = productNmae;
+    }
+
+    public String getQuality() {
+        return Quality;
+    }
+
+    public void setQuality(String quality) {
+        Quality = quality;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String price) {
+        Price = price;
+    }
+
+    public String getDiscount() {
+        return Discount;
+    }
+
+    public void setDiscount(String discount) {
+        Discount = discount;
+    }
 }
