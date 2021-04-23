9f3d51d7127c8448b7cbb57157e5df475f6bf7be
harshitapandey07 <pandey.harshita0708@gmail.com>
2021-04-24 01:25:19 +0530

    updated

--- app/src/main/java/com/hv/briskybake/Database/Database.java
+++ app/src/main/java/com/hv/briskybake/Database/Database.java
@@ -5,6 +5,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -16,10 +17,43 @@ import java.util.List;
 
 public class Database extends SQLiteOpenHelper {
 
-    private static final String DB_NAME="BriskybakeDB.db";
-    private static final int DB_VER=1;
+    private static final String DB_NAME="BriskybakeDB.db";  //Database name
+    private static final int DB_VER=3;  // Database Version
+    private static final String TABLE_NAME = "OrderDetail";   // Table Name
+
+    private static final String UID="_id"; // Column I (Primary Key)
+    private static final String ProductNAME = "ProductName";
+    private static final String ProductID = "ProductId";
+    private static final String QUANTITY= "Quantity";
+    private static final String PRICE= "Price";
+    private static final String DISCOUNT= "Discount";
+    private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME+"( "+UID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+ProductID+" TEXT, "+ProductNAME+" TEXT, "+QUANTITY+" TEXT, "+PRICE+" TEXT, "+DISCOUNT+" TEXT );";
+    private static final String DROP_TABLE="DROP TABLE IF EXISTS "+TABLE_NAME;
+    private Context context;
+
     public Database(@Nullable Context context) {
         super(context, DB_NAME,null,DB_VER);
+        this.context=context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            db.execSQL(CREATE_TABLE);
+        } catch (Exception e) {
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try {
+            Toast.makeText(context, "On Upgrade", Toast.LENGTH_SHORT).show();
+            db.execSQL(DROP_TABLE);
+            onCreate(db);
+        }catch (Exception e) {
+            Toast.makeText(context, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 
     public List<Order> getCarts()
@@ -59,13 +93,5 @@ public class Database extends SQLiteOpenHelper {
         db.execSQL(query);
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
 
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-    }
 }
